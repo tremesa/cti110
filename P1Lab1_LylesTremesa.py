18a6848ebc2116edcce602cb3caaa0a3b1e18352
tremesa <160450186+tremesa@users.noreply.github.com>
2024-02-18 22:32:59 -0500

    Add files via upload
    First Submission

---
+++ P1Lab1_LylesTremesa.py
@@ -0,0 +1,23 @@
+
+print('Tremesa Lyles')
+print('Feburary 18,2024')
+print('P1Lab1')
+print('Python program using IDLE')
+print()
+print()
+
+
+x = "Tremesa"
+print('Hey',(x))
+print('Welcome to zyBooks!')
+
+
+
+
+
+
+
+
+
+
+
